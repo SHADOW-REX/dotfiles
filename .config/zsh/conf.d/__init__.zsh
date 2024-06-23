53bf54b1864ff0a13a8cd752af08f3584340f0f7
SHADOW <33841315+SHADOW-REX@users.noreply.github.com>
2024-06-23 17:41:54 +0530

    Add zsh config

---
+++ .config/zsh/conf.d/__init__.zsh
@@ -0,0 +1,45 @@
+#
+# __init__: This runs prior to any other conf.d contents.
+#
+
+# Apps
+export EDITOR=nvim
+export VISUAL=code
+export PAGER=less
+
+# Set the list of directories that cd searches.
+cdpath=(
+  $XDG_PROJECTS_DIR(N/)
+  $XDG_PROJECTS_DIR/shado(N/)
+  $cdpath
+)
+
+# Set the list of directories that Zsh searches for programs.
+path=(
+  # core
+  $path
+
+  # emacs
+  $HOME/.emacs.d/bin(N)
+  $XDG_CONFIG_HOME/emacs/bin(N)
+
+  # keg only brew apps
+  $HOMEBREW_PREFIX/opt/curl/bin(N)
+  $HOMEBREW_PREFIX/opt/go/libexec/bin(N)
+  $HOMEBREW_PREFIX/share/npm/bin(N)
+  $HOMEBREW_PREFIX/opt/ruby/bin(N)
+  $HOMEBREW_PREFIX/lib/ruby/gems/*/bin(N)
+  $HOME/.gem/ruby/*/bin(N)
+)
+
+# fpath=(
+#   $HOMEBREW_PREFIX/share/zsh/functions(N)
+#   $HOMEBREW_PREFIX/share/zsh/site-functions(N)
+#   $fpath
+# )
+
+# Enable less wait time between key presses.
+export KEYTIMEOUT=1
+
+# Use `< file` to quickly view the contents of any file.
+[[ -z "$READNULLCMD" ]] || READNULLCMD=$PAGER
